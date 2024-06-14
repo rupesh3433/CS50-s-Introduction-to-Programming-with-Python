b78c0c88e1360b1ab80e65a43872e6cdf314ede8
Rupesh Poudel <poudelrupace@gmail.com>
2024-06-14 12:21:35 +0545

    editing twttr.py to make testable function shorten()

--- Problem set 2 Loops/Twttr/twttr.py
+++ Problem set 2 Loops/Twttr/twttr.py
@@ -1,11 +1,16 @@
+def main():
+    input_twits = input("Input: ").strip()
+    print(f"Output: {shorten(input_twits)}")
 
-input_twits = input("Input: ").strip()
-output_twits = ""
+def shorten(input_twits):
+    output_twits = ""
+    vowel = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}
+    for letter in input_twits:
+        if letter in vowel:
+            continue
+        else:
+            output_twits += letter
+    return output_twits
 
-vowel = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}
-for letter in input_twits:
-    if letter in vowel:
-        continue
-    else:
-        output_twits += letter
-print(f"Output: {output_twits}")
+if __name__ == "__main__":
+    main()
